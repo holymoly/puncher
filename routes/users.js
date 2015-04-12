8ebe6648c2d7faac94f75072426151b3a2b0bf44
Maurice Streubel <holzmolz@googlemail.com>
2015-04-12 16:55:48 +0200

    jslint modifications

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
+  /*jslint unparam:true*/
   res.send('respond with a resource');
 });
 
